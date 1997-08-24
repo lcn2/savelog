2b5212606262acc3746ea8072211e76a472d8da4
Landon Curt Noll <github-mail@asthe.com>
1997-08-24 19:31:11 +0000

    use .gz extension, check executable locations, avoid symlinks

--- RCS/OLD/savelog.sh
+++ RCS/OLD/savelog.sh
@@ -21,7 +21,7 @@
 #	-g group  - chgrp log files to group
 #	-c cycle  - save cycle versions of the logfile	(default: 7)
 #	-t	  - touch file
-#	-l	  - don't compress any log files	(default: gzip)
+#	-l	  - don't compress any log files	(default: gzip files)
 #	file 	  - log file names
 #
 # The savelog command saves and optionally compresses old copies of files
@@ -30,7 +30,7 @@
 #
 # Older version of 'file' are named:
 #
-#		OLD/'file'.<number><compress_suffix>
+#		OLD/'file'.<number>.gz
 #
 # where <number> is the version number, 0 being the newest.  By default,
 # version numbers > 0 are compressed (unless -l prevents it). The
@@ -71,26 +71,76 @@
 # Note: Since the version numbers start with 0, version number <cycle>
 #       is never formed.  The <cycle> count must be at least 2.
 #
+# Note: The default compression extension is .gz instead of .z.
+#
+# Note: This utility will not refuse to process symlinks in addition
+#	to other non-file/special-files.
+#
 # Bugs: If a process is still writing to the file.0 and savelog
 #	moved it to file.1 and compresses it, data could be lost.
 
 # common locations
-PATH="/sbin:/usr/sbin:/usr/bin:/bin:/usr/contrib/bin"
-COMPRESS="/usr/sbin/gzip"
-COMP_FLAG="-best"
-DOT_Z=".z"
+#
+PATH="/usr/sbin:/usr/bsd:/sbin:/usr/bin:/bin:/usr/etc:/usr/freeware/bin:/usr/gnu/bin:/usr/local/bin"
+ECHO="/sbin/echo"
+GZIP="/usr/sbin/gzip"
+COMP_FLAG="--best"
+DOT_Z=".gz"
 CHOWN="/sbin/chown"
 CHGRP="/sbin/chgrp"
 CHMOD="/sbin/chmod"
 TOUCH="/sbin/touch"
 MV="/sbin/mv"
 RM="/sbin/rm"
-ECHO="/sbin/echo"
 EXPR="/sbin/expr"
 MKDIR="/sbin/mkdir"
 GETOPT="/usr/bin/getopt"
 
+# paranoid firewall
+#
+if [ ! -x "$ECHO" ]; then
+	echo "$prog: cannot file echo executable: $ECHO" 1>&2
+	exit 1
+fi
+if [ ! -x "$GZIP" ]; then
+	$ECHO "$prog: cannot file gzip executable: $GZIP" 1>&2
+	exit 2
+fi
+if [ ! -x "$CHOWN" ]; then
+	$ECHO "$prog: cannot file chown executable: $CHOWN" 1>&2
+	exit 3
+fi
+if [ ! -x "$CHGRP" ]; then
+	$ECHO "$prog: cannot file chgrp executable: $CHGRP" 1>&2
+	exit 4
+fi
+if [ ! -x "$TOUCH" ]; then
+	$ECHO "$prog: cannot file touch executable: $TOUCH" 1>&2
+	exit 5
+fi
+if [ ! -x "$MV" ]; then
+	$ECHO "$prog: cannot file mv executable: $MV" 1>&2
+	exit 6
+fi
+if [ ! -x "$RM" ]; then
+	$ECHO "$prog: cannot file rm executable: $RM" 1>&2
+	exit 7
+fi
+if [ ! -x "$EXPR" ]; then
+	$ECHO "$prog: cannot file expr executable: $EXPR" 1>&2
+	exit 8
+fi
+if [ ! -x "$MKDIR" ]; then
+	$ECHO "$prog: cannot file mkdir executable: $MKDIR" 1>&2
+	exit 9
+fi
+if [ ! -x "$GETOPT" ]; then
+	$ECHO "$prog: cannot file getopt executable: $GETOPT" 1>&2
+	exit 10
+fi
+
 # parse args
+#
 exitcode=0	# no problems to far
 prog="$0"
 mode=
@@ -102,7 +152,7 @@ compress=1
 set -- `$GETOPT m:o:u:g:c:lt $*`
 if [ $# -eq 1 -o $? -ne 0 ]; then
 	$ECHO "usage: $prog [-m mode][-o owner][-g group][-t][-c cycle][-l] file ..." 1>&2
-	exit 1
+	exit 11
 fi
 for i in $*; do
 	case "$i" in
@@ -118,7 +168,7 @@ for i in $*; do
 done
 if [ "$count" -lt 2 ]; then
 	$ECHO "$prog: count must be at least 2" 1>&2
-	exit 2
+	exit 12
 fi
 
 # cycle thru filenames
@@ -128,10 +178,19 @@ while [ $# -gt 0 ]; do
 	filename="$1"
 	shift
 
-	# catch bogus files
-	if [ -b "$filename" -o -c "$filename" -o -d "$filename" ]; then
+	# catch bogus non-files
+	#
+	# We avoid dealing with symlinks as well ... one should
+	# savelog the actual file, not the symlink
+	#
+	if [ ! -f "$filename" ]; then
 		$ECHO "$prog: $filename is not a regular file" 1>&2
-		exitcode=3
+		exitcode=13
+		continue
+	fi
+	if [ -l "$filename" ]; then
+		$ECHO "$prog: $filename is symlink" 1>&2
+		exitcode=14
 		continue
 	fi
 
@@ -142,7 +201,7 @@ while [ $# -gt 0 ]; do
 			$TOUCH "$filename"
 			if [ "$?" -ne 0 ]; then
 				$ECHO "$prog: could not touch $filename" 1>&2
-				exitcode=4
+				exitcode=15
 				continue
 			fi
 			if [ ! -z "$user" ]; then 
@@ -169,19 +228,19 @@ while [ $# -gt 0 ]; do
 		$MKDIR -p "$savedir"
 		if [ $? -ne 0 ]; then
 			$ECHO "$prog: could not mkdir $savedir" 1>&2
-			exitcode=5
+			exitcode=16
 			continue
 		fi
 		$CHMOD 0755 "$savedir"
 	fi
 	if [ ! -d "$savedir" ]; then
 		$ECHO "$prog: $savedir is not a directory" 1>&2
-		exitcode=6
+		exitcode=17
 		continue
 	fi
 	if [ ! -w "$savedir" ]; then
 		$ECHO "$prog: directory $savedir is not writable" 1>&2
-		exitcode=7
+		exitcode=18
 		continue
 	fi
 
@@ -218,7 +277,7 @@ while [ $# -gt 0 ]; do
 		else
 			newfile="$newname.1$DOT_Z"
 			$RM -f "$newname"
-			$COMPRESS $COMP_FLAG < "$newname.0" > "$newfile"
+			$GZIP $COMP_FLAG < "$newname.0" > "$newfile"
 			$RM -f "$newname.0"
 		fi
 		if [ ! -z "$user" ]; then 
